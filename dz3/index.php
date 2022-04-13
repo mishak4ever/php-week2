fd0ec0a78ec4aebf566ba3b19d12a3204fbfb18a
Михаил <mishak4ever@gmail.com>
2022-04-13 10:13:05 +0500

    подчистил код

--- dz3/index.php
+++ dz3/index.php
@@ -5,8 +5,6 @@ require('src/functions.php');
 $json = task1();
 file_put_contents("users.json", $json);
 $new_json = file_get_contents("users.json");
-//task2($new_json);
-//print_r($new_json);
 echo '<pre>';
    print_r(task2($new_json));
 echo '</pre>';
